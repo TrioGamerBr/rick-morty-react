1363cd049016c8826d77c4fb972a4a05a383764d
TrioGamerBr <triogamer134@gmail.com>
2021-10-30 20:04:31 -0400

    refatoração das rotas

---
+++ src/Routes.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+
+import Home from "./Pages/Home";
+import Personagens from './Pages/Personagens'
+import Personagem from './Pages/Personagem'
+
+function Routes(){
+    return(
+        <Router>
+            <Switch>
+                <Route path="/" exact>
+                    <Home />
+                </Route>
+                <Route path="/personagem/:id">
+                    <Personagem />
+                </Route>
+                <Route path="/personagens">
+                    <Personagens />
+                </Route>
+            </Switch>
+        </Router>
+    );
+}
+export default Routes;
